2898a78f63ad8864c919d619389d0a8614c55af4
Xiao  Tianrui <tianrui.xiao.ray@gmail.com>
2016-03-01 12:50:09 -0500

    Starting MongoDB utils

--- data_utils.py
+++ data_utils.py
@@ -3,6 +3,9 @@ Contains utilities for loading and preprocessing data from database
 """
 import os
 import gensim
+import pymongo
+from pymongo import MongoClient
+from attrdict import AttrDict
 
 
 def load_data_sentences(dirname):
@@ -20,4 +23,18 @@ def train_dynamic(batch_size=10):
     Extract webpage information from each page,
     add as a batch of training data
     """
+    
     return
+
+def fetch_product_list(host_string, product_database, product_collection):
+    product_list = []
+    client = MongoClient(host_string)
+    db = client[product_database]
+    product_posts = db[product_collection]
+
+    for product_post in product_posts.find():
+        product = AttrDicti({'prodID': product_post['product'],
+                             'tags': product_post['tags']})
+        product_list.append(product)
+    return product_list
+    
--- tagmodel.py
+++ tagmodel.py
@@ -10,7 +10,7 @@ class W2Vmodel():
         self.model = models.Word2Vec()
         return
 
-    def construct_model(self, min_count=5, size=200, workers=1):
+    def construct_word_model(self, min_count=5, size=200, workers=1):
         sentence_list = load_data_sentences(self.datadir)
         self.model = models.Word2Vec(sentence_list[0], min_count=min_count, size=size, workers=workers)
         for sentencefile in sentence_list[1:]:
@@ -39,16 +39,11 @@ class W2Vmodel():
             else:
                 # Insert if similarity is higher than the top products
                 if (similarity > top_products[-1][1]):
-                    top_products.remove(top_products[-1])
                     top_products.append((product, similarity))
                     top_products.sort(key=lambda item: item[1])
                     top_products.reverse()
         
         return top_products
-
-    def construct_text8(self):
-        self.model = models.word2vec.Text8Corpus(self.modelfname)
-        return
 
     def get_product_list(self):
         return
